32b4104de72864a702f738d2582303c716595ce2
Roman Nep <roman.nep@electroneek.com>
2022-06-21 13:12:04 +0400

    window size remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:oktopadbooker/app.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
-import 'package:desktop_window/desktop_window.dart';
-
 
 void main() {
   runApp(const MyApp());
-  DesktopWindow.setWindowSize(Size(900, 750));
 }
 
 class MyApp extends StatelessWidget {
